92754b6a6b230ab8937b677d57b824b847570184
yehonatan604 <97249220+yehonatan604@users.noreply.github.com>
2022-10-19 15:01:28 +0300

    V0.05 - 19/10/22 - NavBar Ready

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
   collapsed: boolean = true;
-
+  @Output() featureSelected = new EventEmitter<string>();
   constructor() { }
-
-  ngOnInit(): void {
+  
+  onSelect(selection: string) {
+    this.featureSelected.emit(selection);
   }
-
 }
